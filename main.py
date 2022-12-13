92646169f073ba09967a603681f0a611fc4e454b
Utyujin <utyu.virtual@gmail.com>
2022-12-14 00:33:36 +0900

    Third commit

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import twitter_api as twi
+import time
 
 args = twi.get_args()
 key_path = args["key_path"]
 
 utyu = twi.Twitter_api(key_path = key_path)
 utyu.load_keys()
+
+if t.expiration_time<=time.time()+60:
+    t.refresh()
+    print("refresh")
 
 print(utyu.tweet("にゃーん"))
