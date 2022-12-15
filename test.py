90a38decb3b9196476ba2198f7506e65cf703f0e
Utyujin <utyu.virtual@gmail.com>
2022-12-15 16:31:41 +0900

    remove test

--- test.py
+++
@@ -1,9 +0,0 @@
-import twitter_api as twi
-
-utyu = twi.Twitter_api()
-
-utyu.username = "bocchithegacha"
-utyu.redirect_uri = "https://utyujin.cf"
-utyu.client_id = "b3ZoMU10cHFVOXBodVRzYmp0emg6MTpjaQ"
-
-utyu.add_user()
