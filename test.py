3147d0eec170663d2dd1fea773d8a66873b4b3ea
Utyujin <utyu.virtual@gmail.com>
2022-12-15 16:31:23 +0900

    test

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import twitter_api as twi
 
-utyu = twi.Twitter_api("nktssbat")
+utyu = twi.Twitter_api()
 
+utyu.username = "bocchithegacha"
 utyu.redirect_uri = "https://utyujin.cf"
 utyu.client_id = "b3ZoMU10cHFVOXBodVRzYmp0emg6MTpjaQ"
 
